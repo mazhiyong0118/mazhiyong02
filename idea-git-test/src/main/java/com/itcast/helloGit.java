38d17d4e7969b449556dbb69b7cb0b83e7e1a7ab
mazhiyong <879711002@qq.com>
2019-07-27 18:49:11 +0800

    添加到本地git

--- idea-git-test/src/main/java/com/itcast/helloGit.java
+++ idea-git-test/src/main/java/com/itcast/helloGit.java
@@ -10,5 +10,10 @@ public class helloGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+
     }
 }
